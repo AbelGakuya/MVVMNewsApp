8ee0d0be0d4ad8571bb1a6b3c52e88c5e01d556f
AMG <abelgakuya@gmail.com>
2023-06-01 17:40:29 +0300

    Handled Pagination with retrofit

--- app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -5,5 +5,6 @@ class Constants {
         const val API_KEY = "ea1427d0e0ec49c8adb3932d06e1b153"
         const val BASE_URL = "https://newsapi.org"
         const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
     }
 }
